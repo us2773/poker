a7f9324876b27cd4fecea099503df7d0e56bf34f
us2773 <breeze.prunus0707@gmail.com>
2026-01-13 22:35:15 +0900

    1ターン分のプレイ可能

--- app/main.py
+++ app/main.py
@@ -1,22 +1,29 @@
 from app import main_app
 from flask import render_template, request
-from .modules import gameViewModel
+from .modules import gameViewModel, GameClass
+
+vm = gameViewModel.GameViewModel(GameClass.Game(1))
 
 @main_app.route("/", methods=["GET", "POST"])
 def main() :
-    vm = gameViewModel.GameViewModel()
+    print(f"main.py show_hand: {vm.show_hand}")
     if request.method == "POST":
         action = request.form.get("action")
 
         if action == "show":
+            print("action==show")
+            # 表示ボタンを押した際の処理
             vm.reveal_deals()
 
         elif action == "exchange":
+            # 交換ボタンを押した際の処理
             choice = request.form.getlist("choice")
             vm.exchange_deals(choice)
     
+    print(f"main.py show_hand: {vm.show_hand}")
     return render_template(
         "main.html",
         message=vm.global_message(),
         hand_items=vm.post_deals()
-)
+    )
+    
